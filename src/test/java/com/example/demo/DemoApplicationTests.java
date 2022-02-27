0706f4aa98faae579d9af9a58e0e6a475522c8d8
Nirmala-25 <96299458+Nirmala-25@users.noreply.github.com>
2022-02-28 02:34:29 +0530

    Update DemoApplicationTests.java

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,15 @@
 package com.example.demo;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootTest
-class DemoApplicationTests {
+@SpringBootApplication
+public class book {
 
-	@Test
-	void contextLoads() {
+	public static void main(String[] args)
+	{
+		SpringApplication.run(book.class, args);
+		System.out.println("Hello na public")
 	}
 
 }
